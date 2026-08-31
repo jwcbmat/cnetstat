82caa6ac1a4c9fd540f0f422520798b10ce15cc4
vitordalca <vitordalca@gmail.com>
2026-08-31 17:45:48 -0300

    started sqlite use

--- src/storage/storage.c
+++ src/storage/storage.c
@@ -1,11 +1,13 @@
 #include "storage.h"
 #include "../server/server.h"
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <limits.h>
 #include <errno.h>
+#include <sqlite3.h>
 
 #define LOG(s) fputs(s, stderr)
 
@@ -49,6 +51,17 @@ int wipe_backup() {
   return 0;
 }
 
+void db_start(const char *cwd) {
+  LOG("Creating database... \n");
+  printf("%s", cwd);
+  sqlite3 *db = NULL;
+  char data_path[PATH_MAX];
+  snprintf(data_path, sizeof(data_path), "%s/database", cwd);
+  sqlite3_open(data_path, &db);
+  char query[] = "CREATE TABLE data_test(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)";
+  sqlite3_exec(db, query, 0, 0,0);
+}
+
 void storage_start() {
     LOG("Starting storage... \n");
     char *cwd = getcwd(NULL, 0);
@@ -56,6 +69,7 @@ void storage_start() {
         perror("getcwd() error");
         exit(1);
     }
+    db_start(cwd);
 
     char backup_path[PATH_MAX];
     snprintf(backup_path, sizeof(backup_path), "%s/backup", cwd);
