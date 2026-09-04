efaa012d8b12a4a17e4b1ba37f3e49ff15f551fa
vitordalca <vitordalca@gmail.com>
2026-09-04 08:33:15 -0300

    test: testing db functionalities in main function

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 
 #include <linux/limits.h>
 #include <stdio.h>
+#include <string.h>
+
+char option[10];
 
 int main()
 {
@@ -19,6 +22,19 @@ int main()
     restore_from_backup();
     backup_data();
    */
+    do {
+      printf("Would you like to add a new message? [y/n]: ");
+      if (fgets(option, sizeof(option), stdin) != NULL) {
+        option[strcspn(option, "\n")] = 0;
+        if (strcmp(option, "y") == 0) {
+          char nmessage[256];
+          printf("What message would you like to add: ");
+          if (fgets(nmessage, sizeof(nmessage), stdin) != NULL){
+            post_new_message(nmessage);
+          }
+        };
+      }
+    } while (strcmp(option, "n") != 0);
 
     printf("hello, world! \n");
     return 0;
